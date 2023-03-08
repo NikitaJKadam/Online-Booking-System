aec32df86252550c6f25bb44dd19030eab2bcd50
unknown <nikitajkadam9998@gmail.com>
2023-03-08 14:40:34 +0530

    user service

---
+++ backEnd/user-service/app/models/service-center.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+module.exports = (mongoose) => {
+  var serviceUserSchema = mongoose.Schema({
+    _id: String,
+    password: String,
+    centerName: String,
+    centerContactNo: Number,
+    userRole: String,
+    scBranch: String,
+    avgRating: String,
+    scAddress: {
+        state: String,
+        city: String,
+        street: String,
+        pinCode: Number
+    },
+    productCategory: [],    
+    });
+
+  const ServiceCenter = mongoose.model("ServiceCenter", serviceUserSchema, "ServiceCenter");
+  return ServiceCenter;
+};
